dc598072d6fc7c5b95decab46c1bb5fe7bb65f36
Will Greenberg <ifnspifn@gmail.com>
2024-11-24 15:41:42 -0800

    use gh action

--- docs/_app/immutable/entry/start.BLiZcp9N.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B3fVHZvz.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			base: '/badcrypto',
-			assets: 'docs',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/badcrypto' : '',
-    },
+		adapter: adapter(),
 	}
 };
